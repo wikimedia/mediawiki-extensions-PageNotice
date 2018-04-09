743a7b44af7de507cbb1215f5a72d92faf3b502a
zoranzoki21 <zorandori4444@gmail.com>
2018-04-09 14:03:14 +0200

    Convert PageNotice to use extension registration
    Bug: T174657
    Change-Id: I99fe1c4267317f2186d38b50a069d3d8db45658a

--- PageNotice.php
+++ PageNotice.php
@@ -1,47 +1,14 @@
 <?php
-/**
- * PageNotice extension - lets you define a fixed header or footer message for each page or namespace.
- *
- * Page notices (headers and footers) are maintained as MediaWiki-messages.
- * For page Foo, MediaWiki:top-notice-Foo and MediaWiki:bottom-notice-Foo can be used to defined a header
- * or footer respectively. For namespace 6, MediaWiki:top-notice-ns-6 and MediaWiki:bottom-notice-ns-6 can
- * be used to defined a header or footer respectively. Mind the capitalization.
- *
- * For more info see https://mediawiki.org/wiki/Extension:PageNotice
- *
- * @file
- * @ingroup Extensions
- * @author Daniel Kinzler, brightbyte.de
- * @copyright © 2007 Daniel Kinzler
- * @licence GNU General Public Licence 2.0 or later
- */
-
-if( !defined( 'MEDIAWIKI' ) ) {
-	echo( "This file is an extension to the MediaWiki software and cannot be used standalone.\n" );
-	die( 1 );
+if ( function_exists( 'wfLoadExtension' ) ) {
+	wfLoadExtension( 'PageNotice' );
+	// Keep i18n globals so mergeMessageFileList.php doesn't break
+	$wgMessagesDirs['PageNotice'] = __DIR__ . '/i18n';
+	wfWarn(
+		'Deprecated PHP entry point used for the PageNotice extension. ' .
+		'Please use wfLoadExtension instead, ' .
+		'see https://www.mediawiki.org/wiki/Extension_registration for more details.'
+	);
+	return;
+} else {
+	die( 'This version of the PageNotice extension requires MediaWiki 1.29+' );
 }
-
-$wgExtensionCredits['other'][] = array(
-	'path'           => __FILE__,
-	'name'           => 'PageNotice',
-	'author'         => 'Daniel Kinzler',
-	'url'            => 'https://mediawiki.org/wiki/Extension:PageNotice',
-	'descriptionmsg' => 'pagenotice-desc',
-);
-
-// Disable notices for individual pages, and only allow namespace-wide notices?
-$wgPageNoticeDisablePerPageNotices = false;
-
-$dir = __DIR__;
-$wgAutoloadClasses['PageNoticeHooks'] = $dir . '/PageNotice.hooks.php';
-$wgMessagesDirs['PageNotice'] = __DIR__ . '/i18n';
-$wgHooks['ArticleViewHeader'][] = 'PageNoticeHooks::renderHeader';
-$wgHooks['ArticleViewFooter'][] = 'PageNoticeHooks::renderFooter';
-
-$wgResourceModules['ext.pageNotice'] = array(
-   'styles' => array(
-       'modules/ext.pageNotice.css',
-   ),
-   'localBasePath' => $dir,
-   'remoteExtPath' => 'extensions/PageNotice',
-);
