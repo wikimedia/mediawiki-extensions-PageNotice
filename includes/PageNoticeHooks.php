3a91f2133f3f5d832f27a1870b763eabb7efb501
Zoranzoki21 <zorandori4444@gmail.com>
2020-01-10 21:36:44 +0100

    Add phpcs and make it pass
    Change-Id: I9bc92d2954214004db88eecfb9984cdfc2c42b13

--- includes/PageNoticeHooks.php
+++ includes/PageNoticeHooks.php
@@ -6,15 +6,15 @@
  * @ingroup Extensions
  * @author Daniel Kinzler, brightbyte.de
  * @copyright © 2007 Daniel Kinzler
- * @licence GNU General Public Licence 2.0 or later
+ * @license GPL-2.0-or-later
  */
 
 class PageNoticeHooks {
 	/**
 	 * Renders relevant header notices for the current page.
-	 * @param Article $article
-	 * @param bool $outputDone
-	 * @param bool $pcache
+	 * @param Article &$article
+	 * @param bool &$outputDone
+	 * @param bool &$pcache
 	 * @return bool
 	 */
 	public static function renderHeader( Article &$article, &$outputDone, &$pcache ) {
